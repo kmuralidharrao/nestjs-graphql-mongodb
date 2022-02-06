8f6d76ecb019d63c2ffe3618f3ea2ac207948270
kmuralidharrao <muralidharraok15@gmail.com>
2022-02-06 18:44:22 +0530

    Init commit

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,57 @@
+import { Parent } from '@nestjs/graphql';
+import { ResolveField } from '@nestjs/graphql';
+import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
+import { StudentService } from 'src/student/student.service';
+import { AssignStudentsToLessonInput } from './assign-student-to-lesson.input';
+import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  /* old way
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }*/
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
+  }
+}
---
+++ src/student/student.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(
+    createStudentinput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentinput;
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+
+  async getStudents(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
+
+  async getStudent(id: string): Promise<Student> {
+    return this.studentRepository.findOne({ id });
+  }
+
+  async getManyStudents(studentIds: string[]): Promise<Student[]> {
+    return this.studentRepository.find({
+      where: {
+        id: {
+          $in: studentIds,
+        },
+      },
+    });
+  }
+}
